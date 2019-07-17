444b53fada5c708e422596d0fe63d4716982b6ad
Leary93 <leroyodk@gmail.com>
2019-07-17 19:22:45 +0200

    First version, seems to be working Now commiting to github
    Signed-off-by: Leary93 <leroyodk@gmail.com>

---
+++ ArchFix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Kingmaker;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.ActivatableAbilities;
+
+namespace ArchaeologistFix
+{
+    static class ArchFix
+    {
+        static LibraryScriptableObject library => Main.library;
+
+        static BlueprintActivatableAbility archaeologistLuck;
+        
+        public static void Load()
+        {
+            var library = Main.library;
+
+            archaeologistLuck = library.Get<BlueprintActivatableAbility>("12dc796147c42e04487fcad3aaa40cea");
+
+            archaeologistLuck.Group = ActivatableAbilityGroup.BardicPerformance;
+        }
+    }
+}
